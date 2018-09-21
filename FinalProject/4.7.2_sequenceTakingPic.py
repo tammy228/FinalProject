1cd9ebce3431e9f41e8a588904bbb29da4a941f3
ytliang97 <ytliang97@gmail.com>
2018-09-21 17:17:33 +0800

    desktop crop face ok

---
+++ FinalProject/4.7.2_sequenceTakingPic.py
@@ -0,0 +1,32 @@
+import time
+import picamera
+import os, os.path
+
+
+
+try:
+    frames = 10
+	data_path = '/home/pi/FinalProject/picture'
+
+    with picamera.PiCamera() as camera:
+        camera.resolution = (1024, 768)
+        camera.framerate = 30
+        camera.start_preview()
+        # Give the camera some warm-up time
+        time.sleep(2)
+        start = time.time()
+		
+        camera.capture_sequence([
+            'data_path/image%02d.jpg' % i
+            for i in range(frames)
+            ], use_video_port=True)
+        finish = time.time()
+    print('Captured %d frames at %.2ffps' % (
+        frames,
+        frames / (finish - start)))
+    camera.close()
+except KeyboardInterrupt:
+    pass
+    camera.close()
+except:
+camera.close()
