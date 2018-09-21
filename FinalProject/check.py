5293c0bcc54286b26b0f8b14c1c5c09a65e6784e
ytliang97 <ytliang97@gmail.com>
2018-09-21 16:49:50 +0800

    update check.py add facedetect

--- FinalProject/check.py
+++ FinalProject/check.py
@@ -2,6 +2,7 @@ import os
 import shutil
 from os.path import join
 from testModel import recognizeFace
+from facedetect_dlib import findface_dlib_forTakingPic
 
 data_path = '/home/pi/FinalProject/picture'
 
@@ -9,8 +10,11 @@ while os.listdir(data_path):
 	pic_name = os.listdir(data_path)
 	for i in pic_name :	
 		fullpath = join(data_path,i)
+		name = findface_dlib_forTakingPic(fullpath, i)
+	'''
+	for i in pic_name :	
+		fullpath = join(data_path,i)
 		name = recognizeFace(fullpath)
 		print("This person is: %s" %(name))
 		shutil.move(fullpath,"/home/pi/FinalProject/moved_picture")
-
-
+	'''
