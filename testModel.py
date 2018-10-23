9e9aa8b839bc1cbfdd08a8478c98f58ab46c8814
tammy228 <saintpaulstammy@gmail.com>
2018-10-23 11:11:09 +0800

    model finally wrok ~~~~~~~~~~~

--- testModel.py
+++ testModel.py
@@ -6,27 +6,29 @@ from keras.preprocessing.image import load_img ,img_to_array
 from testModelFun import read_name_list
 from time import time
 
-def recognizeFace(path,model):
+def recognizeFace(path):
+	img_width, img_height = 150, 150
+	model = load_model('/media/tammy/T/FinalProject_Local/eighteenPeople_model2_bin_v1.h5')
 	img = load_img(path,target_size=(150,150))
-        print("img")
-        print(img)
 	x = img_to_array(img)
-        print("img_to_array")
-        print(x)
+	
+	#img.save('test.png')
+	#img.show()
+	x = cv2.cvtColor(x, cv2.COLOR_BGR2GRAY)
 	x = np.expand_dims(x, axis=0)
-        print("expand_dims")
-        print(x)
+	x = np.expand_dims(x, axis=1)
+	if x.shape == (1,1, img_width, img_height):
+		x.shape = (1,img_width, img_height, 1)
 	#x = preprocess_input(x)
-	x = x.astype('float32')
-        print("float")
-        print(x)
+#	x = x.astype('float32')
 	x = x/255.0
-        print("divide by 255")
-        print(x)
 
 	prob = model.predict(x)
 	max_index = np.argmax(prob)
-	name_list = read_name_list('/home/pi/FinalProject/data/test')
+	name_list = read_name_list('/media/tammy/T/FinalProject_Local/data/test')
+	print("Everyone's Probility:")
+	for i in range(0, max_index+1):
+		print(str(name_list[i])+":"+str(prob[0][i]))
 	if prob[0][max_index] > 0.5:
 	    show_name = name_list[max_index]
 	else:
