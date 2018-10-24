8bb3920b2d0ed47dc3c10f828c7ada287ee33367
tammy228 <saintpaulstammy@gmail.com>
2018-10-24 17:26:17 +0800

    tammy idiot

--- testModelFun.py
+++ testModelFun.py
@@ -2,8 +2,10 @@ import os
 import numpy as np
 
 def read_name_list(path):
-    name_list = []
-    for child_dir in os.listdir(path):
-        name_list.append(child_dir)
-    return name_list
-
+	name_list = []
+	result = os.listdir(path)
+	result.sort()
+	for child_dir in result:
+		name_list.append(child_dir)
+	
+	return name_list
